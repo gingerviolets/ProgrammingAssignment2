4035bf3b853ae11ee5c80797746e1f22a0cf2876
gingerviolets <55763622+gingerviolets@users.noreply.github.com>
2022-05-22 12:48:05 -0400

    Update cachematrix.R
    Submission for Week 3 of R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## required per Assignment 2 for Week 3 of R Programming, this set of
+##functions caches the inverse of an existing matrix, then returns that inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix : sets & gets elements of given matrix of x, then its
+## inverse to be called
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # nolint
+        inverted <- NULL
+        set <- function(y) {
+                x <<- y
+                inverted <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inverted <<- inverse # nolint # nolint
+        getinverse <- function() inverted # nolint
+        list(get = get, set = set,
+            setinverse = setinverse,
+            getinverse = getinverse)
 }
 
+## cacheSolve: 1) checks for existing cached data for inverted; returns inverted
+## 2) else, calculates inverse of matrix as inverted; returns inverted
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { # nolint
+        inverted <- x$getinverse()
+        if(!is.null(inverted)) {
+        message("getting cached data")
+        return(inverted)
+}
+x_matrix <- x$get()
+inverted <- solve(x_matrix, ...)
+x$setinverse(inverted)
+inverted
 }
